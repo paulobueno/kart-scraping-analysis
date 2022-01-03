9749247591c59d43bdc60f8d88b9a76f2e0052bf
Paulo Bueno <paulob.bruno@gmail.com>
2022-01-02 23:47:24 -0300

    update packages

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from scraping.scrap import KgvCollectData
 
 if __name__ == '__main__':
-    kgv_data = KgvCollectData(date_range=('2019-05-01', '2019-12-31'))
+    kgv_data = KgvCollectData(date_range=('2021-01-01', '2021-12-31'))
     kgv_data(to_csv='./Data/data.csv')
 
