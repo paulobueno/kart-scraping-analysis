457b2d5026652b9dcea475331657dbf04ab8cfb0
Paulo Bueno <paulob.bruno@gmail.com>
2024-01-11 23:57:18 -0300

    delete main

--- main.py
+++
@@ -1,6 +0,0 @@
-from scraping.scrap import KgvCollectData
-
-if __name__ == '__main__':
-    kgv_data = KgvCollectData(date_range=('2021-01-01', '2021-12-31'))
-    kgv_data(to_csv='./Data/data.csv')
-
